9455db50765e8a10a7a80037d061e8682cb95468
memandip <mandip810@gmail.com>
2017-12-24 12:15:01 +0545

    student added to firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from './components/includes/Navbar';
 import { Sidebar } from './components/includes/Sidebar';
 import { Footer } from './components/includes/Footer';
 import { StudentAddForm } from './components/student/add';
+import {Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 import './bower_components/bootstrap/dist/css/bootstrap.min.css';
 import './bower_components/font-awesome/css/font-awesome.min.css';
@@ -13,27 +14,20 @@ import './dist/css/skins/_all-skins.min.css';
 class App extends Component {
   render() {
     return (
-        <div className="wrapper">
-            <Navbar />
-            <Sidebar />
-            <div className="content-wrapper">
-                {/*<section className="content-header">*/}
-                    {/*<h1>*/}
-                        {/*Dashboard*/}
-                        {/*<small>Control panel</small>*/}
-                    {/*</h1>*/}
-                    {/*<ol className="breadcrumb">*/}
-                        {/*<li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>*/}
-                        {/*<li className="active">Dashboard</li>*/}
-                    {/*</ol>*/}
-                {/*</section>*/}
-
-                <section className="content">
-                    <StudentAddForm />
-                </section>
+        <Router>
+            <div className="wrapper">
+                <Navbar />
+                <Sidebar />
+                <div className="content-wrapper">
+                    <section className="content">
+                        <Switch>
+                            <Route exact path="/" component={StudentAddForm} />
+                        </Switch>    
+                    </section>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </Router>
     );
   }
 }
